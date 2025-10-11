852f65774e82f3af3e923cf3d9872776798f02ed
pacific006 <146131662+pacific006@users.noreply.github.com>
2025-10-11 21:58:27 +0530

    Update AiModel.jsx

--- src/service/AiModel.jsx
+++ src/service/AiModel.jsx
@@ -11,7 +11,7 @@ export async function main(FINAL_PROMT) {
     responseMimeType: "application/json",
   };
   // const model = "gemini-2.5-flash-preview-04-17";
-  const model = "models/gemini-1.5-pro";
+  const model = "gemini-2.5-flash";
   const contents = [
     {
       role: "user",
